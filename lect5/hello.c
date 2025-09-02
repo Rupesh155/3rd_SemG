db7d6fa62350f85a513de36127dffc04a36aca3d
Rupesh155 <rudhg35@gmail.com>
2025-09-02 14:03:32 +0530

    loops

---
+++ lect5/hello.c
@@ -0,0 +1,47 @@
+
+#include <stdio.h>
+
+int main(){
+    // printf("hello");
+    // printf("hello");
+    // printf("hello");
+    // printf("hello");
+    // for(int i =0;i<=5;i++){
+    //     // printf("hello\n");
+    //     printf("%d\n",i);
+    // }
+    // for(int i =10;i>0;i--){
+    //     // printf("%d\n",i);
+    //     if(i%2==1){
+    //          printf("%d\n",i);
+    //     }
+    // }
+    //  int sum=0;
+    // for(int i =1;i<=5;i++){
+    //     sum=sum+i;
+    //     // sum+=i;
+
+      
+    // }
+    //   printf("%d\n",sum);
+
+    // int i=0;
+    // while (i<4)
+    // {
+    //     printf("%d",i);
+    //     i++;
+    //     /* code */
+    // }
+    int num=123;
+    int sum=0;
+    while (num>0)
+    {
+       int lastDigit=num%10;
+       sum=sum+lastDigit;
+       num=num/10;
+    }
+    printf("%d",sum);
+    
+    
+
+}
