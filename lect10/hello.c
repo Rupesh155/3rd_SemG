ec01077d313ae7f568f9fd517a5fa364de54305a
Rupesh155 <rudhg35@gmail.com>
2025-09-10 12:51:22 +0530

    full butterfly

--- lect10/hello.c
+++ lect10/hello.c
@@ -16,4 +16,21 @@ int main(){
         printf("\n");
     }
 
+    for(int i=0;i<4;i++){
+        for(int j=3;j>i;j--){
+            printf("*");
+        }
+        for(int k=0;k<=i;k++){
+            printf(" ");
+        }
+         for(int k=0;k<=i;k++){
+            printf(" ");
+        }
+         for(int j=3;j>i;j--){
+            printf("*");
+        }
+        printf("\n");
+
+    }
+
 }
