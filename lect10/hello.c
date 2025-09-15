980b94edbceeace21147c945df10a6c249cc65c3
Rupesh155 <rudhg35@gmail.com>
2025-09-15 16:02:15 +0530

    pattern

--- lect10/hello.c
+++ lect10/hello.c
@@ -1,36 +1,175 @@
 #include <stdio.h>
+  
+
 int main(){
-    for(int i =0;i<4;i++){
-        for(int j=0;j<=i;j++){
-            printf("*");
-        }
-        for(int k=3;k>i;k--){
-            printf(" ");
-        }
-         for(int p=3;p>i;p--){
-            printf(" ");
-        }
-        for(int z=0;z<=i;z++){
-            printf("*");
+    int n=5;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            // printf("*");
+            if(i==1 || i==n || j==1 || j==n){
+                printf("*");
+            }else{
+                printf(" ");
+            }
         }
         printf("\n");
     }
 
-    for(int i=0;i<4;i++){
-        for(int j=3;j>i;j--){
-            printf("*");
-        }
-        for(int k=0;k<=i;k++){
-            printf(" ");
-        }
-         for(int k=0;k<=i;k++){
-            printf(" ");
-        }
-         for(int j=3;j>i;j--){
-            printf("*");
-        }
-        printf("\n");
-
-    }
 
 }
+
+    // for(int i=0;i<4;i++){
+    //       for(int j=0;j<=i;j++){
+    //         printf("*");
+    //     }
+       
+    //     for(int k=3;k>i;k--){
+    //         printf(" ");
+    //     }
+    //       for(int k=3;k>i;k--){
+    //         printf(" ");
+    //     }
+    //     for(int p=0;p<=i;p++){
+    //         printf("*");
+    //     }
+    //     printf("\n");
+
+
+
+    // }
+
+
+//     for(int i=0;i<4;i++){
+//         for(int j=3;j>i;j--){
+//             printf("*");
+//         }
+//         for(int k=0;k<=i;k++){
+//             printf(" ");
+//         }
+//           for(int k=0;k<=i;k++){
+//             printf(" ");
+//         }
+//           for(int j=3;j>i;j--){
+//             printf("*");
+//         }
+//         printf("\n");
+
+//     }
+
+   
+
+// }
+    /*int a,b=0,c;
+      printf("Enter Number to check:");
+      scanf("%d",&a);
+      if(a==1){
+        printf("Not prime");
+      }
+      else{}
+      for(int i=2; i<a/2;i++)
+      {
+        if(a%i==0){
+            b++;
+        }
+      }
+      if(b!=0){
+        printf("number is not prime");
+      }
+      else{
+        printf("number is prime")
+      }
+       return 0;*/
+
+//        int n;
+//        printf("enter number:");
+//        scanf("%d",&n);
+//        for(int i=2;i<=n;i++){
+//         int count=0;
+//         for(int j=1;j<=i;j++)
+//          if(i%j==0)
+//           count++;
+//         if(count==2)
+//          printf("%d\n",i);
+//        }
+//        return 0;
+
+// int n=122
+// ;
+// int b=n;
+// for(;n>0;n=n/10)
+// { 
+//    int  ld= n%10;
+//    int num=10*ld;
+// }
+
+// (n==b)?printf("is palindrome"):printf("not");
+
+// }
+
+    
+
+    
+
+
+
+//     int n=5;
+//     for(int i =1;i<=n;i++){
+//         for(int j=1;j<=n;j++){ 
+//             // printf("*");
+//             if(i==1 || i==n || j==1|| j==n){
+//                 printf("*");
+//             }else{
+//                 printf(" ");
+//             }
+//         }
+//         printf("\n");
+//     }
+// }
+
+
+
+
+
+
+
+
+// int main(){
+//     for(int i =0;i<4;i++){
+//         for(int j=0;j<=i;j++){
+//             printf("*");
+//         }
+//         for(int k=3;k>i;k--){
+//             printf(" ");
+//         }
+//          for(int p=3;p>i;p--){
+//             printf(" ");
+//         }
+//         for(int z=0;z<=i;z++){
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+
+//     for(int i=0;i<4;i++){
+//         for(int j=3;j>i;j--){
+//             printf("*");
+//         }
+//         for(int k=0;k<=i;k++){
+//             printf(" ");
+//         }
+//          for(int k=0;k<=i;k++){
+//             printf(" ");
+//         }
+//          for(int j=3;j>i;j--){
+//             printf("*");
+//         }
+//         printf("\n");
+
+//     }
+
+
+
+
+
+
+
