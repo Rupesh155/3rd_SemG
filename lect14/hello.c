9ac9e579b31f6bb449131a7e751adc27b3db55e1
Rupesh155 <rudhg35@gmail.com>
2025-09-17 13:47:19 +0530

    sliding window

--- lect14/hello.c
+++ lect14/hello.c
@@ -7,24 +7,38 @@
 #include <limits.h>
 
 int maxArraySum(int arr[] ,int n,int k){
-    int maxSum=INT_MIN;
-        // int arr[7]={-1,2,4,5,-4,3,-7};
-    for(int i=0;i<=n-k;i++){
-        int currSum=0;
-        for(int j=i;j<i+k;j++){
-            currSum=currSum+arr[j];
-
-        }
-        if(currSum>maxSum){
-            maxSum=currSum;
+    int wSum=0;
+    //   int arr[7]={-1,2,4,5,4,3,7};
+    for( int i=0;i<k;i++){
+        wSum= wSum+arr[i];
+    }
+    int maxSum=wSum;
+    for( int j=k;j<n;j++){
+        wSum=wSum+arr[j]-arr[j-k];
+        if(wSum>maxSum){
+            maxSum=wSum;
         }
     }
     return maxSum;
 
+    // int maxSum=INT_MIN;
+    //     // int arr[7]={-1,2,4,5,-4,3,-7};
+    // for(int i=0;i<=n-k;i++){
+    //     int currSum=0;
+    //     for(int j=i;j<i+k;j++){
+    //         currSum=currSum+arr[j];
+
+    //     }
+    //     if(currSum>maxSum){
+    //         maxSum=currSum;
+    //     }
+    // }
+    // return maxSum;
+
 
 }
 int main(){
-    int arr[7]={-1,2,4,5,-4,3,-7};
+    int arr[7]={-1,2,4,5,4,3,7};
     int k=3;
      int n=    sizeof(arr)/sizeof(arr[0]);
   int data=     maxArraySum(arr,n,k);
@@ -33,3 +47,7 @@ int main(){
 
 
 }
+
+
+    // int arr[7]={1,2,4,5,4,3,7};
+    // int k=3
